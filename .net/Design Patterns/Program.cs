39063ec34456279f6e8aab3667c492465a64d9fe
sdinesh999 <35901397+sdinesh999@users.noreply.github.com>
2020-01-21 14:34:24 +0530

    Add files via upload

---
+++ .net/Design Patterns/Program.cs
@@ -0,0 +1,14 @@
+    namespace SingletonDemo
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Singleton fromTeachaer = Singleton.GetInstance;
+                fromTeachaer.PrintDetails("From Teacher");
+                Singleton fromStudent = Singleton.GetInstance;
+                fromStudent.PrintDetails("From Student");
+                Console.ReadLine();
+            }
+        }
+    }
