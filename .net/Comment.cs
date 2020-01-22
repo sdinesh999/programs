f70b6dc2243c851873291e4912566448b8b030e2
sdinesh999 <35901397+sdinesh999@users.noreply.github.com>
2020-01-22 17:45:36 +0530

    Add files via upload

---
+++ .net/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyFirstProject1
+{
+    class Comment
+    {
+        /// <summary>
+        /// multiplying two numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int MulNumbers(int a,int b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(MulNumbers(2, 3));
+        }
+    }
+}
