b8b4382c8e393d2504e6bb3a9705734f83a33493
sdinesh999 <35901397+sdinesh999@users.noreply.github.com>
2020-01-29 11:21:36 +0530

    Add files via upload

---
+++ memory_management/MemoryUse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    /// <summary>
+    /// deallocating resources by 'using' keyword
+    /// </summary>
+    class MemoryUse
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Use of using Keyword";
+            // To release the resources manually
+            #region 
+            MyResourceClass ms1 = new MyResourceClass();
+            try
+            {
+                ms1.DoSomeTask();
+            }
+            finally
+            {
+                ms1.Dispose();
+            }
+            #endregion
+            // To release the resources automatically by 'using' keyword 
+            #region   
+            using (MyResourceClass ms = new MyResourceClass())
+            {
+                ms.DoSomeTask();
+            }
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
---
+++ memory_management/MyResourceClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    /// <summary>
+    /// manually releasing the resources by using dispose method
+    /// </summary>
+    class MyResourceClass : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing all the resources......");
+        }
+        public void DoSomeTask()
+        {
+            Console.WriteLine("Starting some task......");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Doing some task.....");
+            }
+        }
+    }
+}
