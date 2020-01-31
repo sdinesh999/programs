447408eda34db599ff044e45971a534d7093505d
sdinesh999 <35901397+sdinesh999@users.noreply.github.com>
2020-01-31 11:25:49 +0530

    Add files via upload

---
+++ custom collections/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquatableDemo
+{/// <summary>
+/// checking the data using equatable 
+/// </summary>
+    class Employee
+    {
+        public string Account;
+        public string Name;
+        public Employee(string account, string name)
+        {
+            this.Account = account;
+            this.Name = name;
+        }
+        public bool Equals(Employee other)
+        {
+            if (other == null) return false;
+            return (this.Account.Equals(other.Account));
+        }
+    }
+}
---
+++ custom collections/EquatableMain.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquatableDemo
+{
+    class EquatableMain
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("sai", "Geert Verhoeven");
+            ArrayList employees = new ArrayList();
+            employees.Add(emp1);
+            Employee emp2 = new Employee("sai", "Geert");
+
+            // Comparing with an object from the same class.
+            Console.WriteLine("emp1.Equals(emp2): {0}", emp1.Equals(emp2));
+            // Comparing with an object from a different class.
+            Object obj = emp2;
+            Console.WriteLine("emp1.Equals(obj): {0}", emp1.Equals(obj));
+            //  Using an ArrayList
+            Console.WriteLine("employees.Contains(emp2): {0}",
+            employees.Contains(emp2));
+            Console.ReadLine();
+        }
+    }
+}
